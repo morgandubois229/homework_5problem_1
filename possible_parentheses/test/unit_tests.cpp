0be6fcc764cfff707302ede4979f7c51beb4cd90
morgandubois229 <mgdb@uw.edu>
2019-11-16 17:29:58 -0800

    Tests initialized in unit_tests.cpp

--- possible_parentheses/test/unit_tests.cpp
+++ possible_parentheses/test/unit_tests.cpp
@@ -7,31 +7,71 @@ void showResult(std::list<std::string> & result) {
     }
 };
 
+bool isEqual(std::list<std::string> & toCheck, std::list<std::string> & answer) {
+    bool returnValue = true;
+
+    if (toCheck.size() != answer.size()) {
+        returnValue = false;
+        return returnValue;
+    }
 
 
-/*
+    while (toCheck.empty() == false) {
+
+        if (toCheck.front() == answer.front()) {
+            // Pop top of both stacks
+            toCheck.pop_front();
+            answer.pop_front();
+        }
+        else {
+            returnValue = false;
+            break;
+        }
+    }
+
+    return returnValue;
+}
+
+TEST(test_recursive, 0) {
+    std::list<std::string> result;
+    possibleParenthesis(0, result);
+
+    std::list<std::string> answer;
+
+    ASSERT_TRUE(isEqual(result, answer));
+}
+
 TEST(test_recursive, 1) {
     std::list<std::string> result;
     possibleParenthesis(1, result);
 
-    // homework: add verification
+    std::list<std::string> answer;
+    answer.push_front("{}");
+
+    ASSERT_TRUE(isEqual(result, answer));
 }
 
 TEST(test_recursive, 2) {
     std::list<std::string> result;
     possibleParenthesis(2, result);
 
-    // homework: add verification
+    std::list<std::string> answer;
+    answer.push_back("{}{}");
+    answer.push_back("{{}}");
+
+    ASSERT_TRUE(isEqual(result, answer));
 }
-*/
 
 TEST(test_recursive, 3) {
     std::list<std::string> result;
     possibleParenthesis(3, result);
-    showResult(result);
 
-    // homework: add verification
-    ASSERT_TRUE(true);
+    std::list<std::string> answer;
+    answer.push_front("{}{}{}");
+    answer.push_back("{}{{}}");
+    answer.push_back("{{}}{}");
+    answer.push_back("{{}{}}");
+    answer.push_back("{{{}}}");
+
+    ASSERT_TRUE(isEqual(result, answer));
 }
-
-// and more
